684cb0582f1e748c0779926a78860b560b3c20ad
Austen Bright <pureaustenbright@gmail.com>
2020-03-28 20:16:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,72 @@
-# My Code here....
+# My Code here...
+def map_to_negativize(source_array)
+  arr = []
+  i = 0
+  while i < source_array.length do
+    arr << source_array[i] * -1
+    i += 1
+  end
+  arr
+end
+
+def map_to_no_change(source_array)
+  arr = []
+  i = 0
+  while i < source_array.length do
+    arr << source_array[i]
+    i += 1
+  end
+  arr
+end
+
+def map_to_double(source_array)
+  arr = []
+  i = 0
+  while i < source_array.length do
+    arr << source_array[i] * 2
+    i += 1
+  end
+  arr
+end
+
+def map_to_double(source_array)
+  arr = []
+  i = 0
+  while i < source_array.length do
+    arr << source_array[i] * 2
+    i += 1
+  end
+  arr
+end
+
+def map_to_square(source_array)
+  arr = []
+  i = 0
+  while i < source_array.length do
+    arr << source_array[i] ** 2
+    i += 1
+  end
+  arr
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  i = 0
+  total = starting_point
+  while i < source_array.length
+    total += source_array[i]
+    i += 1
+  end
+  total
+end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    if source_array[i] == nil || source_array[i] == false
+      false
+    else
+      true
+    end
+    i += 1
+  end
+end
