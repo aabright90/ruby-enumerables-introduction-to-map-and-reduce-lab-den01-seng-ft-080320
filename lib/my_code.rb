2cb8ad4e401d5a95f3e3f4235277c23f222feb12
Austen Bright <pureaustenbright@gmail.com>
2020-03-28 20:27:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -74,9 +74,9 @@ def reduce_to_any_true(source_array)
   i = 0
   while i < source_array.length do
     if source_array[i] == true
-      true
+      return true
     end
     i += 1
   end
-  false
+  return false
 end
